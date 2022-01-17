59f7b2dd7e69125e6313d46955c2b9d6e5719ca0
Masterchief-07 <kjonathan1999@hotmail.com>
2022-01-17 00:15:29 +0000

    fix bug

--- User.cpp
+++ User.cpp
@@ -1,7 +1,7 @@
 #include "User.hpp"
 
 
-User::User(std::string name, double amount ,Blockchain *block):_name(name), _amount(amount), _chain(block) 
+User::User(std::string name, double amount ,Blockchain& block):_name(name), _amount(amount), _chain(&block) 
 {
 	std::time_t ti = time(0);
 	_id = std::hash<std::string>{}(name + std::to_string(ti));
--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@
 int main()
 {
 	Blockchain block;
-	User jonathan{std::string{"jonathan"}, 1000.f, &block};
-	User josue{std::string{"josue"}, 1000.f, &block};
+	User jonathan{std::string{"jonathan"}, 1000.f, block};
+	User josue{std::string{"josue"}, 1000.f, block};
 	std::cout<<"jonathan amount : "<<jonathan.GetAmount()<<"\n";
 	std::cout<<"josue amount : "<<josue.GetAmount()<<"\n";
 	jonathan.Send(10, josue);
